f470051bf249e1f21bed44aa4f753c72f2f3ab53
YourGuruNet <arvisiljins@gmail.com>
2024-03-15 16:05:27 +0200

    search routing fix

--- frontend/auction-house-web/src/Components/Navbar/SearchBar/Search.js
+++ frontend/auction-house-web/src/Components/Navbar/SearchBar/Search.js
@@ -3,13 +3,19 @@ import React, { useState } from "react";
 import "./search.scss";
 import { IoIosSearch } from "react-icons/io";
 import { useParamsStore } from "@/app/Hooks/UseParamStore";
+import { usePathname, useRouter } from "next/navigation";
 
 const Search = () => {
+  const router = useRouter();
+  const pathname = usePathname();
   const setParams = useParamsStore((state) => state.setParams);
   const setSearch = useParamsStore((state) => state.setSearch);
   const searchValue = useParamsStore((state) => state.searchValue);
   const handleSearch = () => {
     setParams({ searchTerm: searchValue });
+    if (pathname !== "/") {
+      router.push("/");
+    }
   };
   return (
     <div className="search-container">
