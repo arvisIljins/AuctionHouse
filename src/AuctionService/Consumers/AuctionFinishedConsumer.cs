62389622293fca943c55e8d0322f892875569d6a
YourGuruNet <arvisiljins@gmail.com>
2024-03-21 20:54:00 +0200

    guid fix

--- src/AuctionService/Consumers/AuctionFinishedConsumer.cs
+++ src/AuctionService/Consumers/AuctionFinishedConsumer.cs
@@ -15,7 +15,7 @@ namespace AuctionService.Consumers
 
         public async Task Consume(ConsumeContext<AuctionFinished> context)
         {
-            var auction = await _context.Auctions.FindAsync(context.Message.AuctionId);
+            var auction = await _context.Auctions.FindAsync(Guid.Parse(context.Message.AuctionId));
 
             if(auction is null) return;
 
