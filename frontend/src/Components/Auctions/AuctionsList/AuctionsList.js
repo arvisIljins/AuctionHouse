6c865ed1b75e629735c375d0bb92060951c996a5
YourGuruNet <arvisiljins@gmail.com>
2024-04-02 20:53:24 +0300

    created useAuctionStore

--- frontend/src/Components/Auctions/AuctionsList/AuctionsList.js
+++ frontend/src/Components/Auctions/AuctionsList/AuctionsList.js
@@ -10,12 +10,10 @@ import { useParamsStore } from "@/app/Hooks/UseParamStore";
 import { shallow } from "zustand/shallow";
 import queryString from "query-string";
 import Loader from "@/Components/Loader/Loader";
+import { useAuctionStore } from "@/app/Hooks/UseAuctionStore";
 
 const AuctionsList = () => {
-  const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [pageCount, setPageCount] = useState(0);
-
   const params = useParamsStore(
     (state) => ({
       pageNumber: state.pageNumber,
@@ -29,31 +27,42 @@ const AuctionsList = () => {
     shallow
   );
 
+  const data = useAuctionStore(
+    (state) => ({
+      auctions: state.auctions,
+      totalCount: state.totalCount,
+      pageCount: state.pageCount,
+    }),
+    shallow
+  );
+
   const url = queryString.stringifyUrl({ url: "", query: params });
+
+  const setData = useAuctionStore((state) => state.setData);
 
   useEffect(() => {
     setData([]);
     getData(url).then((result) => {
-      setData(result.data.items);
-      setPageCount(result.data.pageCount);
+      setData(result.data);
       setLoading(false);
     });
   }, [url]);
 
-  const showList = !loading && data.length > 0;
+  const showList = !loading && data.auctions.length > 0;
+  console.log(data);
   return (
     <>
       <Filters />
       {loading && <Loader />}
       {showList && (
         <div className="list">
-          {map(data, (auction) => {
+          {map(data.auctions, (auction) => {
             return <AuctionCard key={auction.title} auction={auction} />;
           })}
         </div>
       )}
       {!showList && <h1 className="no-results">There is no result found</h1>}
-      {showList && <Pagination pageCount={pageCount} />}
+      {showList && <Pagination pageCount={data.pageCount} />}
     </>
   );
 };
---
+++ frontend/src/app/Hooks/UseAuctionStore.js
@@ -0,0 +1,26 @@
+import { create } from "zustand";
+
+const initialState = {
+  auctions: [],
+  totalCount: 0,
+  pageCount: 0,
+};
+
+export const useAuctionStore = create((set) => ({
+  ...initialState,
+  setData: (data) => {
+    set(() => ({
+      auctions: data.items,
+      totalCount: data.totalCount,
+      pageCount: data.pageCount,
+    }));
+  },
+  setCurrentPrice: (auctionId, amount) =>
+    set((state) => ({
+      auctions: state.auctions.map((auction) =>
+        auction.id == auctionId
+          ? { ...auction, currentHightBill: amount }
+          : auction
+      ),
+    })),
+}));
