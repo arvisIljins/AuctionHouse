75208a69554e334fd95ba3c62ee8a53e0628bd30
YourGuruNet <arvisiljins@gmail.com>
2024-04-10 20:51:37 +0300

    Error fixes

--- frontend/src/app/constants.js
+++ frontend/src/app/constants.js
@@ -1,5 +1,9 @@
+export const idProd = process.env.NODE_ENV === "production";
+
 export const urls = {
   issuer: process.env.API_ISSUER,
   baseUrl: process.env.API_URL,
-  notificationsUrl: process.env.NEXT_PUBLIC_API_NOTIFICATIONS,
+  notificationsUrl: idProd
+    ? "https://api.auctions-house.com/notifications"
+    : process.env.NEXT_PUBLIC_API_NOTIFICATIONS,
 };
