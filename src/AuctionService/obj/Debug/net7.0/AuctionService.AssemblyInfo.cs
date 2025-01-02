77d5c2a05410c10444aa37c7439d63ff9e8f7cd3
YourGuruNet <arvisiljins@gmail.com>
2025-01-02 15:53:17 +0200

    a

--- src/AuctionService/obj/Debug/net7.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net7.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ea48a797f26d05750113f007783e1af2e956877")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3ae5fd383dbba1c1062325d58cbc5d399a5ffe3")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
